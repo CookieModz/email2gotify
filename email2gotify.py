ac14f86e8a34f01f3da9876937b5f03f810385f4
4oo4 <4oo4@users.noreply.github.com>
2021-10-11 20:03:36 -0500

    Update to python 3.x

--- email2gotify.py
+++ email2gotify.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import base64
 import email
@@ -30,7 +31,7 @@ def decode_field(field_raw):
         return field_raw
 
 def debug(debug_type, debug_msg):
-    print "debug(%d): %s" % (debug_type, debug, msg)
+    print(f"{debug_type} {debug} {msg}")
 
 subject_raw = msg.get('Subject', '')
 subject = decode_field(subject_raw)
@@ -41,27 +42,20 @@ body_text = ''
 for part in msg.walk():
     if part.get_content_type() == 'text/plain':
         body_part = part.get_payload()
-        if part.get('Content-Transfer-Encoding') == 'base64':
-            body_part = base64.decodestring(body_part)
-        part_encoding = part.get_content_charset()
-        if part_encoding:
-            body_part = body_part.decode(part_encoding)
-        else:
-            body_part = body_part.decode()
 
         if body_text:
-            body_text = '%s\n%s' % (body_text, body_part)
+          body_text = body_text + '\n' + body_part
         else:
             body_text = body_part
 
-body_text = '%s\nFrom: %s' % (body_text, sender)
-body_text = unicodedata.normalize('NFKD',body_text).encode('ascii','ignore')
+body_text = body_text + '\n' + sender
 
 push_headers = {
     "title": subject,
     "message": body_text,
     "priority": 5,
 }
+
 push_json = json.dumps(push_headers)
 
 gotify_endpoint = args.url + '/message?token=' + args.key
@@ -83,8 +77,6 @@ http_status = c.getinfo(c.HTTP_CODE)
 fail_status = [400, 401, 403, 404, 500, 502, 503]
 
 if http_status in fail_status or debug_mode:
-  print response
+  print(response)
 
 c.close()
-
-
